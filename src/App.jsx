dd5e460a7bf08d67a93e64905101583e6cb4e023
Shabd Vasudeva <127197800+developer2207@users.noreply.github.com>
2023-12-24 16:22:17 +0000

    abcd

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      <header className='App-header'>
+        <h1>
+          Android Port World
+        </h1>
       </header>
+      <nav>
+        <p>
+          here you'll get all ported applications for you
+        </p>
+      </nav>
+      <footer>
+        <a
+        href="google.com">
+          visit our site
+        </a>
+      </footer>
     </div>
   );
 }
