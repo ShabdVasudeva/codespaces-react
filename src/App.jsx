6988071286be9ee8deb689b8157cc644fa5e088a
Shabd Vasudeva <127197800+developer2207@users.noreply.github.com>
2023-12-25 08:16:35 +0000

    androidPortWorld

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>
-          Android Port World
-        </h1>
-      </header>
-      <nav>
-        <p>
-          here you'll get all ported applications for you
-        </p>
-      </nav>
-      <footer>
-        <a
-        href="google.com">
-          visit our site
-        </a>
-      </footer>
-    </div>
+      <div className="App">
+        <header>
+          <h1>Android Port World</h1>
+          <p>get a lot of ports from here</p>
+        </header>
+        <main>
+          <h1>Application ports</h1>
+          <div className='division2'>
+            <h3>Samsung Ports</h3>
+            <p>Gallery --> <a href="https://www.pling.com/p/2102872/">download</a></p>
+            <p>Keyboard --> <a href="https://www.pling.com/p/2112503/">download</a></p>
+            <p>Home Launcher --> <a href="https://www.pling.com/p/2033463/">download</a></p>
+            <p>Calculator(8.1+) --> <a href="https://www.pling.com/p/2033463/"></a></p>
+            <p>Clock --> <a href="https://www.pling.com/p/2033463/">download</a></p>
+            <p>Notes(old 7.0+) --> <a href="https://t.me/AndroidPortWorld/274">download</a></p>
+          </div>
+          <div className='division2'>
+            <h3>Aosp Apps</h3>
+            <p>Lawnchair(sushi mod) --> <a href="https://www.pling.com/p/2110330/">download</a></p>
+            <p>Dot Os Music --> <a href="https://www.pling.com/p/2033249">download</a></p>
+            <p>Ancient Os Widget --> <a href="https://www.pling.com/p/2033249">download</a></p>
+          </div>
+          <div className='division2'>
+            <h3>FlymeOs 14 Apps</h3>
+            <p>Notes --> <a href='https://www.pling.com/p/2113471/'>download</a></p>
+            <p>ToolBox --> <a href="https://t.me/AndroidPortWorld/809">download</a></p>
+            <p>Clock --> <a href="https://t.me/AndroidPortWorld/803">download</a></p>
+            <p>Calendar --> <a href='https://github.com/developer2207/Flyme-os-13-Calendar'>download</a></p>
+          </div>
+          <div className='division2'>
+            <h3>Nothing Apps</h3>
+            <p>Launcher --> <a href='https://t.me/AndroidPortWorld/763'>download</a></p>
+            <h6>more applications coming soon stay tuned...</h6>
+          </div>
+          <div className='donate'>
+            <h1>donation</h1>
+            <p>help the developer by 
+            <a href='https://t.me/AndroidPortWorld/211'> donate</a>
+            </p>
+          </div>
+        </main>
+       <nav>
+          <p>&copy; 2023 A.P.W. All rights reserved.</p>
+       </nav>
+      </div>
   );
 }
-
 export default App;
